3eb900ac6094b73ae63828a9ba1abf9446b13345
Pedro7242 <pedroalberto123452009@gmail.com>
2026-05-26 12:50:58 -0300

    Update alunoController.ts

--- backend/controllers/alunoController.ts
+++ backend/controllers/alunoController.ts
@@ -1,36 +1,35 @@
-import { Request, Response } from 'express'
 import { AlunoDAO } from '../dao/alunoDAO'
 
 const alunoDAO = new AlunoDAO()
 
 export class AlunoController {
 
-  async listar(req: Request, res: Response) {
+  async listar(req, res) {
     const alunos = await alunoDAO.listarTodos()
     res.json(alunos)
   }
 
-  async buscar(req: Request, res: Response) {
+  async buscar(req, res) {
     const id = Number(req.params.id)
 
     const aluno = await alunoDAO.buscarPorId(id)
 
     if (!aluno) {
       return res.status(404).json({
-        erro: 'Aluno não encontrado'
+        erro: 'aluno n encontrado'
       })
     }
 
     res.json(aluno)
   }
 
-  async criar(req: Request, res: Response) {
+  async criar(req, res) {
     const aluno = await alunoDAO.criar(req.body)
 
     res.status(201).json(aluno)
   }
 
-  async atualizar(req: Request, res: Response) {
+  async atualizar(req, res)  {
     const id = Number(req.params.id)
 
     const aluno = await alunoDAO.atualizar(id, req.body)
@@ -38,13 +37,13 @@ export class AlunoController {
     res.json(aluno)
   }
 
-  async deletar(req: Request, res: Response) {
+  async deletar(req, res) {
     const id = Number(req.params.id)
 
     await alunoDAO.deletar(id)
 
     res.json({
-      mensagem: 'Aluno deletado'
+      mensagem: 'aluno deletado'
     })
   }
 }
