193bc439007db9a2c6766763e28a4301fa0f4c7b
Pedro7242 <pedroalberto123452009@gmail.com>
2026-05-30 18:21:02 -0300

    Update alunoRoute.ts

--- backend/routes/alunoRoute.ts
+++ backend/routes/alunoRoute.ts
@@ -1,1 +1,13 @@
+import { Router } from 'express';
+import { AlunoController } from '../controllers/alunoController.js';
 
+const router = Router();
+const alunoController = new AlunoController();
+
+router.get('/', alunoController.listar);
+router.get('/:id', alunoController.buscar);
+router.post('/', alunoController.criar);
+router.put('/:id', alunoController.atualizar);
+router.delete('/:id', alunoController.deletar);
+
+export default router;
