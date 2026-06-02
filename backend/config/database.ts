8a76019868616c0d3075cdff862bb76b0fad17c3
Pedro7242 <pedroalberto123452009@gmail.com>
2026-06-02 10:31:06 -0300

    Create database.ts

---
+++ backend/config/database.ts
@@ -0,0 +1,5 @@
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaClient } from "../generated/prisma/client";
+const adapter = new PrismaBetterSqlite3({ url: './dev.db' });
+const prisma = new PrismaClient({ adapter, log: ['query'] });
+export { prisma };
